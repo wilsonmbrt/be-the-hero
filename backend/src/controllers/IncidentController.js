7016aa18cd63e8fae8620c051aa758fcaa4220ed
Wilson Marcos <wilsonmb02@gmail.com>
2020-03-26 15:18:30 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -21,7 +21,7 @@ module.exports = {
 
         res.header('X-Total-Count', count['count(*)'])
         
-        return res.json({incidents})
+        return res.json(incidents)
     },
 
     async create(req, res) {
